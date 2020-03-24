cfcedf0840410e9c7f45b7e8fc8292d241a54342
NeilSCGH <neil.segard@gmail.com>
2020-03-24 12:28:33 +0100

    success money money money

--- Session 3/Q_Learning.py
+++ Session 3/Q_Learning.py
@@ -5,24 +5,13 @@ file="bank_of_america.csv"
 date = np.loadtxt(fname = file,dtype=str, delimiter=',', skiprows=1,usecols=0)
 close = np.loadtxt(fname = file, delimiter=',', skiprows=1,usecols=4)
 N=len(date)
+states=np.unique(close)
 
-plt.scatter(date, close, marker="x")
-plt.xlabel("X")
-plt.ylabel("Y")
+# plt.scatter(date, close, marker="x")
+# plt.xlabel("X")
+# plt.ylabel("Y")
 #♥plt.show()
 
-
-#initial cash flow in USD
-initialBalance = 5000
-
-
-#initial nb of action
-nbActionsAchetees = 0
-volumeTransaction=5
-
-#Q = portfolio
-Q= np.zeros((N,3))
-#balance + nbActions*valueAction
 
 #A : 3: hold, buy, sell
 
@@ -32,63 +21,80 @@ alpha = 0.05
 #discount factor
 gamma = 0.05
 
-def computeCurrentDD(nb):
-    price=close[-1]
-    return nb*price
 
-def execute(balance,nbActionsAchetees,action,t):
-    price=close[t]
-    totalPrice=volumeTransaction*price
-    newBalance=balance
-    newNbActionsAchetees=nbActionsAchetees
-
-    if action==0 and balance>=totalPrice:#buy
-        newBalance=balance -totalPrice
-        newNbActionsAchetees=nbActionsAchetees+volumeTransaction
-        #assert newBalance>0
-
-    elif action==1 and nbActionsAchetees>=volumeTransaction:#sell
-        newBalance= balance+totalPrice
-        newNbActionsAchetees=nbActionsAchetees-volumeTransaction
-        #assert newNbActionsAchetees>0
-
-    return newBalance,newNbActionsAchetees
-
-def simulatePolicy(p):
-    balance=initialBalance
-    nbActions=0
-    for ordre in range(len(p)):
-        balance,nbActions=execute(balance,nbActions,p[ordre],ordre)
-        #print("bal",balance,"  nbaction",nbActions)
-
-    return balance, nbActions
-
-#updating Q dans un while
 #while pas convergence (convergence = pas d'amelioration)
 Q= np.zeros((N,3))
 
 for i in range(10):
     print(np.round(Q,2))
     print()
-    #tester Q
-    policy=np.argmax(Q,axis=1)
-    newBal,newNbAction=simulatePolicy(policy)
-    DD=computeCurrentDD(newNbAction) #draw down
-    equity=newBal + DD
-    reward = equity - initialBalance
 
-    newQ=[]
     for t in range(0,N-1):
-        #mettre a jour Q
-        newQ.append((1-alpha) * Q[t] + alpha * (reward + gamma * max(Q[t+1])))
+        for a in range(3):
+            if a==0:#buy at t
+                if close[t+1] > close[t]: # si ca monte
+                    reward = 1
+                else:
+                    reward = -1
+            elif a==1:#sell at t
+                if close[t+1] < close[t]: # si ca descend
+                    reward = 1
+                else:
+                    reward = -1
+            elif a==2:#hold between t and t+1
+                if close[t+1] > close[t]: # si ca monte
+                    reward = 1
+                else:
+                    reward = -1
 
-    newQ.append([0,0,0])
-    Q=newQ
+            Q[t][a]=(1-alpha) * Q[t][a] + alpha * (reward + gamma * max(Q[t+1]))
+
+
 
 print(np.round(Q,3))
 policy=np.argmax(Q,axis=1)
 
+balance = 5000
+nbActions=0
+volumeTransaction=10
+bal=[balance]
 
-# t=state=date
-# a=action = buy/sell/hold
-# policy = quelle action faire a chaque instant : P[t]=action
+for t in range(N):
+    action=policy[t]
+    price=close[t]
+
+    volumeTransaction=int(balance/close[t]*0.05)
+    prixTransaction = volumeTransaction*close[t]
+
+    if action == 0 and balance>=prixTransaction:#buy
+        nbActions += volumeTransaction
+        balance -= prixTransaction
+
+    elif action == 1 :#sell
+        balance += nbActions*close[t]
+        nbActions =0
+
+    #elif action == 2:#hold
+        #♣do nothing
+
+    bal.append(balance + nbActions*close[t])
+
+
+print("Final balance")
+print(balance + nbActions*close[N-1])
+
+plt.plot(bal)
+plt.xlabel("T")
+plt.ylabel("balance")
+plt.show()
+
+
+# plt.scatter(date, close, marker="x")
+# buy=(policy==0)*close
+# plt.scatter(date, buy, marker="*")
+# sell=(policy==1)*close
+# plt.scatter(date, sell, marker="o")
+# plt.xlabel("X")
+# plt.ylabel("Y")
+# plt.show()
+
